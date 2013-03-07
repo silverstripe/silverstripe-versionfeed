0353f72e5e596ecffb7127d8011f65ef620100a8
Robert Curry <robert@silverstripe.com>
2013-03-07 15:41:46 +1300

    3.0 compatibility fix

--- _config.php
+++ _config.php
@@ -1,4 +1,4 @@
 <?php
 
-SiteTree::add_extension('VersionFeed');
-ContentController::add_extension('VersionFeed_Controller');
+Object::add_extension('SiteTree', 'VersionFeed');
+SiteTree::add_extension('ContentController', 'VersionFeed_Controller');
