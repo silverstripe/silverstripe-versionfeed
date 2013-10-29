13bb2cf270609f2bb9a1244099fbb366514d74f1
Ingo Schommer <ingo@silverstripe.com>
2013-10-29 12:06:34 +0100

    Travis support

--- tests/VersionFeedTest.php
+++ tests/VersionFeedTest.php
@@ -1,6 +1,17 @@
 <?php
 class VersionFeedTest extends SapphireTest {
 
+	protected $usesDatabase = true;
+
+	protected $requiredExtensions = array(
+		'SiteTree' => array('VersionFeed'),
+		'ContentController' => array('VersionFeed_Controller'),
+	);
+
+	protected $illegalExtensions = array(
+		'SiteTree' => array('Translatable')
+	);
+
 	public function testDiffedChangesExcludesRestrictedItems() {
 		$this->markTestIncomplete();
 	}
@@ -13,9 +24,7 @@ class VersionFeedTest extends SapphireTest {
 		$page = new Page(array('Title' => 'My Title'));
 		$page->write();
 		$page->publish('Stage', 'Live');
-		$feed = new VersionFeed();
-		$feed->setOwner($page);
-
+	
 		$page->Title = 'My Changed Title';
 		$page->write();
 		$page->publish('Stage', 'Live');
@@ -25,13 +34,13 @@ class VersionFeedTest extends SapphireTest {
 
 		$this->assertContains(
 			_t('RSSHistory.TITLECHANGED', 'Title has changed:') . 'My Changed Title',
-			array_map('strip_tags', $feed->getDiffedChanges()->column('DiffTitle')),
+			array_map('strip_tags', $page->getDiffedChanges()->column('DiffTitle')),
 			'Detects published title changes'
 		);
 
 		$this->assertNotContains(
 			_t('RSSHistory.TITLECHANGED', 'Title has changed:') . 'My Unpublished Changed Title',
-			array_map('strip_tags', $feed->getDiffedChanges()->column('DiffTitle')),
+			array_map('strip_tags', $page->getDiffedChanges()->column('DiffTitle')),
 			'Ignores unpublished title changes'
 		);
 	}
